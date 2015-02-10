afa883e6c0ceb74810cb7c15385a7f8657b28527
Ali Essam <ali.essam.arafa@gmail.com>
2015-02-10 22:05:12 +0200

    Initial Refactoring

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,12 @@
 window.onload = init;
-var context;
+var audioContext;
 var analyser;
 var frequencyData;
 
 var canvas = document.querySelector('#cnv');
 var canvasCtx = canvas.getContext('2d');
-var WIDTH = 256;
-var HEIGHT = 64;
+var CANVAS_WIDTH = 256;
+var CANVAS_HEIGHT = 64;
 
 var qasdb = [];
 
@@ -15,142 +15,37 @@ var chatHistoryDiv = document.querySelector('#chat-history');
 var micIcon = document.querySelector('#mic-icon');
 
 var recognition;
-var recognitionStatus = false;
+var recognitionOn = false;
 
 var animationId;
 
-inpTxt.onkeydown = function(e) {
-	if(e.keyCode == 13) {
-		handleInput(inpTxt.value);
-		inpTxt.value = "";
-	}
-}
-
-micIcon.onclick = function() {
-	console.log('micIcon click');
-	if(!recognitionStatus)
-		recognition.start();
-	else
-		recognition.stop();
-}
-
-inpTxt.onblur = function(){
-	inpTxt.focus();
-}
-
-function handleInput(question) {
-	var qa = findQA(question);
-	chatHistoryDiv.innerHTML += "You: " + question + '</br>';
-	if(qa != null) {
-		chatHistoryDiv.innerHTML += "Samantha: " + qa.a + '</br>';
-		playSound('./audio/samantha/'+qa.f+'.ogg');
-	}
-	else {
-		chatHistoryDiv.innerHTML += "Samantha: " + "I don't know!" + '</br>';
-		errNum = Math.floor(Math.random() * 3) + 1;
-		playSound('./audio/samantha/e' + errNum + '.ogg');
-	}
-	chatHistoryDiv.scrollTop = chatHistoryDiv.scrollHeight;
-	
-}
-
 function init() {
 	window.AudioContext = window.AudioContext || window.webkitAudioContext;
-	context = new AudioContext();
+	audioContext = new AudioContext();
+	// TODO: OS Introduction.
 	//playSound('./audio/hello.ogg');
 	loadQAsDBJSON();
 	initSpeechRecognition();
 	recognition.start();
 }
 
-function playSound(path) {
-	var bufferLoader = new BufferLoader(
-		context, [
-			path
-		],
-		finishedLoading
-	);
-	bufferLoader.load();
-}
+// Input handling
 
-
-function initSpeechRecognition() {
-	if (!('webkitSpeechRecognition' in window)) {
-		console.log('speech recognition error');
+function handleInput(question) {
+	var qa = findQA(question);
+	chatHistoryDiv.innerHTML += "You: " + question + '</br>';
+	if (qa != null) {
+		chatHistoryDiv.innerHTML += "Samantha: " + qa.a + '</br>';
+		playSound('./audio/samantha/' + qa.f + '.ogg');
 	} else {
-		recognition = new webkitSpeechRecognition();
-		recognition.continuous = true;
-		recognition.interimResults = false;
-
-		recognition.onstart = function() {
-			recognitionStatus = true;
-			addClass(micIcon, 'pulsating');
-			console.log('recognition started');
-		}
-		recognition.onresult = onSpeechResult;
-		recognition.onerror = function(event) {}
-		recognition.onend = function() {
-			recognitionStatus = false;
-			removeClass(micIcon, 'pulsating');
-			console.log('recognition ended');
-		}
+		chatHistoryDiv.innerHTML += "Samantha: " + "I don't know!" + '</br>';
+		errNum = Math.floor(Math.random() * 3) + 1;
+		playSound('./audio/samantha/e' + errNum + '.ogg');
 	}
+	chatHistoryDiv.scrollTop = chatHistoryDiv.scrollHeight;
 }
 
-function onSpeechResult(event) {
-	var msg = '';
-	for (var i = event.resultIndex; i < event.results.length; i++) {
-		msg+=event.results[i][0].transcript;
-	};
-	console.log(msg);
-	handleInput(msg);
-}
-
-function finishedLoading(bufferList) {
-	var source = context.createBufferSource();
-	analyser = context.createAnalyser();
-	source.buffer = bufferList[0];
-	source.connect(analyser);
-	analyser.connect(context.destination);
-	frequencyData = new Uint8Array(analyser.frequencyBinCount);
-	source.start(0);
-	playing = true;
-	source.onended = function() {cancelAnimationFrame(animationId);}
-	update();
-}
-var H = 0;
-
-function update() {
-	animationId = requestAnimationFrame(update);
-	analyser.getByteFrequencyData(frequencyData);
-
-	var sum = 0;
-
-	for (var i = 0; i < frequencyData.length; i++) {
-		sum += frequencyData[i];
-	}
-	var avg = sum / frequencyData.length;
-	H += 0.07 * avg / 20;
-	drawSpectrum(avg / 80, H);
-}
-
-function drawSpectrum(w, h) {
-	canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-	var amp = w;
-	var disp = h;
-	var wid = 1 / 47;
-
-	for (var dir = -1; dir <= 1; dir += 2) {
-		canvasCtx.beginPath();
-		canvasCtx.lineWidth = 7;
-		canvasCtx.strokeStyle = "rgb(230,230,230)";
-		for (var i = 0; i <= WIDTH; i++) {
-			var y = dir * amp * Math.sin(wid * i + disp);
-			canvasCtx.lineTo(0.5 + i, HEIGHT / 2 - y * HEIGHT / 2); // 
-		}
-		canvasCtx.stroke();
-	}
-}
+// Questions/Answers Database
 
 function loadQAsDBJSON() {
 	var xobj = new XMLHttpRequest();
@@ -165,10 +60,11 @@ function loadQAsDBJSON() {
 }
 
 function findQA(question) {
-	var bestQA = null, bv = 0.3;
-	qasdb.map(function(qa){
+	var bestQA = null,
+		bv = 0.3;
+	qasdb.map(function(qa) {
 		var v = question.fuzzy(qa.q, 0.3);
-		if(v>bv){
+		if (v > bv) {
 			bv = v;
 			bestQA = qa;
 		}
@@ -176,17 +72,140 @@ function findQA(question) {
 	return bestQA;
 }
 
-function hasClass(ele,cls) {
-  return !!ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+// Speech Recognition
+
+function initSpeechRecognition() {
+	if (!('webkitSpeechRecognition' in window)) {
+		// TODO: Alert user (micIcon, text).
+		console.error('Speech Recognition Not Available');
+	} else {
+		recognition = new webkitSpeechRecognition();
+		recognition.continuous = true;
+		recognition.interimResults = false;
+
+		recognition.onstart = function() {
+			recognitionOn = true;
+			addClass(micIcon, 'pulsating');
+			console.log('recognition started');
+		}
+		recognition.onend = function() {
+			recognitionOn = false;
+			removeClass(micIcon, 'pulsating');
+			console.log('recognition ended');
+		}
+
+		recognition.onresult = onSpeechResult;
+		recognition.onerror = onSpeechError;
+	}
 }
 
-function addClass(ele,cls) {
-  if (!hasClass(ele,cls)) ele.className += " "+cls;
+function onSpeechResult(event) {
+	var msg = '';
+	for (var i = event.resultIndex; i < event.results.length; i++) {
+		msg += event.results[i][0].transcript;
+	};
+	console.log(msg);
+	handleInput(msg);
 }
 
-function removeClass(ele,cls) {
-  if (hasClass(ele,cls)) {
-    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
-    ele.className=ele.className.replace(reg,' ');
-  }
+function onSpeechError(event) {
+	console.error(event);
+	// TODO: Handle network and not-allowed errors.
+}
+
+// Sound Play
+
+function playSound(path) {
+	var bufferLoader = new BufferLoader(
+		audioContext, [
+			path
+		],
+		finishedLoadingSound
+	);
+	bufferLoader.load();
+}
+
+function finishedLoadingSound(bufferList) {
+	var source = audioContext.createBufferSource();
+	analyser = audioContext.createAnalyser();
+	source.buffer = bufferList[0];
+	source.connect(analyser);
+	analyser.connect(audioContext.destination);
+	frequencyData = new Uint8Array(analyser.frequencyBinCount);
+	source.start(0);
+	playing = true;
+	source.onended = function() {
+		cancelAnimationFrame(animationId);
+	}
+	updateAnimation();
+}
+
+// Animation
+
+var waveDisp = 0;
+
+function updateAnimation() {
+	animationId = requestAnimationFrame(updateAnimation);
+	analyser.getByteFrequencyData(frequencyData);
+	// TODO: Use volume instead of summing various freqs.
+	var sum = 0;
+	for (var i = 0; i < frequencyData.length; i++) {
+		sum += frequencyData[i];
+	}
+	var avg = sum / frequencyData.length;
+	// Magical Values choosen by fairy creatures.
+	waveDisp += 0.07 * avg / 20;
+	drawSpectrum(avg / 80, waveDisp);
+}
+
+function drawSpectrum(amp, disp) {
+	canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+	var waveZoom = 47;
+	for (var dir = -1; dir <= 1; dir += 2) {
+		canvasCtx.beginPath();
+		canvasCtx.lineWidth = 7;
+		canvasCtx.strokeStyle = "rgb(230,230,230)";
+		for (var i = 0; i <= CANVAS_WIDTH; i++) {
+			var y = dir * amp * Math.sin(1 / waveZoom * i + disp);
+			canvasCtx.lineTo(0.5 + i, CANVAS_HEIGHT / 2 - y * CANVAS_HEIGHT / 2);
+		}
+		canvasCtx.stroke();
+	}
+}
+
+// Event Handlers
+
+inpTxt.onkeydown = function(e) {
+	if (e.keyCode == 13) {
+		handleInput(inpTxt.value);
+		inpTxt.value = "";
+	}
+}
+
+micIcon.onclick = function() {
+	if (!recognitionOn)
+		recognition.start();
+	else
+		recognition.stop();
+}
+
+inpTxt.onblur = function() {
+	inpTxt.focus();
+}
+
+// Helper Functions
+
+function hasClass(ele, cls) {
+	return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+function addClass(ele, cls) {
+	if (!hasClass(ele, cls)) ele.className += " " + cls;
+}
+
+function removeClass(ele, cls) {
+	if (hasClass(ele, cls)) {
+		var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+		ele.className = ele.className.replace(reg, ' ');
+	}
 }
